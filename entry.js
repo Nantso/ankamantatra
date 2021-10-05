b8fcb87dbf0b9fa0d911f2fdaa0ae8020bfbda42
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-10-05 16:51:48 +0300

    add some features

--- entry.js
+++ entry.js
@@ -1,3 +1,14 @@
+const colors = {
+    BLACK: 0,
+    RED: 1,
+    GREEN: 2,
+    YELLOW: 3,
+    BLUE: 4,
+    MANGETA: 5,
+    CYAN: 6,
+    WHITE: 7,
+};
+
 /**
  * @param {any[]} questions
  * @returns {{question: string, answer: string, possiblities:string[]}}
@@ -23,13 +34,13 @@ function checkResponse(response, possiblities) {
  * @returns {string}
  */
 function formatResponse(response) {
-    return response ? "\x1b[42mMARINA\x1b[0m" : "\x1b[41mDISO\x1b[0m";
+    return response ? colorText("MARINA", colors.GREEN, true) : colorText("DISO", colors.RED, true);
 }
 
 /**
  * Create input prompt for getting user input on the console
  * @param {string} message
- * @returns {{ closer: Function, lineReader: any}}
+ * @returns {Promise<string>}
  */
 function input(message = ">", newLine = false) {
     const readline = require("readline");
@@ -50,29 +61,86 @@ function input(message = ">", newLine = false) {
     });
 }
 
+/**
+ * Show loader
+ * @param {string} text
+ * @param {string[]} chars
+ * @param {number} delay
+ * @returns
+ */
 function loadingAnimation(
     text = "",
     chars = ["⠙", "⠘", "⠰", "⠴", "⠤", "⠦", "⠆", "⠃", "⠋", "⠉"],
     delay = 100
 ) {
+    chars = chars.map(c => colorText(c, 5))
     let x = 0;
     return setInterval(function () {
         process.stdout.write("\r" + chars[x++] + " " + text);
         x = x % chars.length;
     }, delay);
 }
+
+/**
+ * Await for a specific delay
+ * @param {number} duration
+ * @returns
+ */
+function sleep(duration) {
+    return new Promise(resolve => {
+        return setTimeout(() => {
+            resolve();
+        }, duration);
+    });
+}
+
+/**
+ * Return preformated text with colors
+ * @param {string} text
+ * @param {number} color A number 0 to 7
+ * @returns {string}
+ */
+function colorText(text, color = 0, bgColor) {
+    let start = 30, max = 37;
+    if (bgColor) {
+        start += 10;
+        max += 10;
+    }
+    return text.split('')
+        .map(t => {
+            if (color > max || start + color > max) {
+                color = start;
+            }
+            return `\x1b[${start + color}m${t}\x1b[0m`
+        }).join('');
+}
+
 async function main() {
+    let play = true;
     const data = require("./ankamantatra.json");
-    const q = getRandomQuestion(data);
-    console.log(`INONA ARY IZAO: ${q.question} ?`);
-    const userInput = await input(); // TODO: Get user input
-    q.possiblities.push(q.answer); // Add the answer to the possiblities list
-    const animation = loadingAnimation()
-    setTimeout(() => {
+    loop: while (play) {
+        const q = getRandomQuestion(data);
+        console.log(`INONA ARY IZAO: ${colorText(q.question, colors.YELLOW)} ?`);
+        let userInput = await input();
+        if (!userInput.length) {
+            continue loop;
+        }
+        q.possiblities.push(q.answer); // Add the answer to the possiblities list
         const response = formatResponse(checkResponse(userInput, q.possiblities));
-        clearInterval(animation);
-        console.log(`\n${response}\n`);
-    }, 1000);
+        const a = loadingAnimation("Miandrasa kely ...");
+        const t = await sleep(1000);
+        clearTimeout(t);
+        clearInterval(a);
+        console.log(`\n${response}`);
+        userInput = await input(`MBOLA HANOY VE IANAO ? ${colorText("(ENY/Tsia)", colors.MANGETA)}\n>`);
+        if (
+            ['t', 'tsia', 'aaa', 'a3', 'tsy', 'ts']
+                .includes(userInput.toLowerCase())
+        ) {
+            console.log(colorText(`VELOMA ✋ !`, colors.BLUE, true));
+            play = false;
+        }
+    }
 }
 
 main();
